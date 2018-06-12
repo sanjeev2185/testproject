0f46c6ea8c83219132cc67eb598342aec5011b10
sanjeev <sanjeev.kumar@eyeforweb.com>
2018-06-12 15:37:58 +0530

    modify

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 
 echo phpinfo();
-
+//https://github.com/sanjeev2185/testproject.git
 ?>
+git remote add origin https://github.com/sanjeev2185/testproject.git
+git push -u origin master
+git push --force origin master
